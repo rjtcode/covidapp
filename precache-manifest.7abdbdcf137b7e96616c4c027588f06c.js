fce6e9c8e4f6f9b95530bd42d4627bbe7c2c5398
rjtcode <rjtsharma5452@gmail.com>
2020-10-13 21:10:40 +0530

    Updates

---
+++ precache-manifest.7abdbdcf137b7e96616c4c027588f06c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e503b8955bd39011dcd255e77dea8a52",
+    "url": "/covidapp/index.html"
+  },
+  {
+    "revision": "e71a3acbbdac604a08a8",
+    "url": "/covidapp/static/css/main.5d6f2cf8.chunk.css"
+  },
+  {
+    "revision": "fb77cb5fe4f905e115d5",
+    "url": "/covidapp/static/js/2.060707f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidapp/static/js/2.060707f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71a3acbbdac604a08a8",
+    "url": "/covidapp/static/js/main.089b01be.chunk.js"
+  },
+  {
+    "revision": "2c9438d00b33ccd43645",
+    "url": "/covidapp/static/js/runtime-main.2e02ef70.js"
+  }
+]);
